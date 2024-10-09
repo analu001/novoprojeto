778b78b0442513515fdca35cb4bfcde8c963a951
analu001 <152643733+analu001@users.noreply.github.com>
2024-10-09 08:43:25 -0300

    yyhty

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criaCartao(
+    'História',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Física',
+    'A que temperatura a água ferve?',
+    '100 graus celsius'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos continentes existem?',
+    'Existem 6'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os 6 continentes?',
+    'América, Europa, África, Ásia, Oceania e Antártida (ou Antártica).'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a maior floresta tropical do mundo?',
+    'Floresta Amazônica'
+)
+
+criaCartao(
+    'História',
+    'Quem pintou Mona Lisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Química',
+    'Qual o nome popular do cloreto de sódio (NaCl)?',
+    'Sal de cozinha'
+)
+
+criaCartao(
+    'História',
+    'Qual a nacionalidade de Napoleão Bonaparte?',
+    'Francesa'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Cristovão Colombo'
+)
+
+criaCartao(
+    'História',
+    'Que grande evento histórico aconteceu em 1822 no Brasil?',
+    'Independência do Brasil'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que região se localiza o estado de Minas Gerais?',
+    'Sudeste'
+)
+
+criaCartao(
+    'Português',
+    'Quem escreveu Os Lusíadas?',
+    'Luís Vaz de Camões'
+)
+
+criaCartao(
+    'Português',
+    'Qual o plural de chapéu?',
+    'Chapéus'
+)
+
+criaCartao(
+    'Português',
+    'Na frase "João passou no concurso" o sujeito é?',
+    'João'
+)
+
+criaCartao(
+    'Português',
+    'Alentar é o mesmo que...?',
+    'animar, encorajar'
+)
+
+criaCartao(
+    'Inglês',
+    'A palavra “college” significa?',
+    'Faculdade'
+)
+
+criaCartao(
+    'Inglês',
+    'Que tempo verbal está expresso na frase “They washed the dishes.”?',
+    'Simple Past'
+)
+
+criaCartao(
+    'Inglês',
+    ' O que significa "I wonder..."?',
+    'Eu gostaria...'
+)
+
+criaCartao(
+    'Inglês',
+    'O que está errado na frase "He were doing the exercises"?',
+    '"Were"'
+)
+
+criaCartao(
+    'Química',
+    'Em qual família o Carbono se encontra?',
+    'Família 14'
+)
